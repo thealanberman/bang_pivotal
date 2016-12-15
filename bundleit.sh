2269379658b258b55176628ae4d22708c786fe07
Al <thealanberman@gmail.com>
2016-12-14 22:05:29 -0800

    made bundleit more robust

--- bundleit.sh
+++ bundleit.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
-zip -9 bundle.zip *.py
-zip -r9 bundle.zip lib/python2.7/site-packages/*
+confirm() { read -sn 1 -p "$1 [Y/N]? "; [[ $REPLY = [Yy] ]]; }
+
+confirm "Are you in the lambda working directory?" && {
+  zip -9 bundle.zip *.py
+  zip -r9 bundle.zip lib/python2.7/site-packages/*
+}
+
+echo "Now run:"
+echo "aws lambda update-function-code --zip-file fileb://bundle.zip --function-name FUNCTION_NAME"
