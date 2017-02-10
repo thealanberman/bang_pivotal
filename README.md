f3af5d675cbfeaff39e003d0d8b628cb0f1d5572
Al <thealanberman@gmail.com>
2017-02-09 23:42:02 -0800

    heavy modifications, but it WORKS

--- README.md
+++ README.md
@@ -1,26 +1,69 @@
 # !pivotal Slack command
+
 Creates the ability to add stories to PivotalTracker from Slack via a `!pivotal` trigger word.
 
 Do you use Slack? Do you use PivotalTracker? Want to be able to quickly add stories to PivotalTracker from Slack?
 
-The code in this repo is designed to run as an [AWS Lambda](https://aws.amazon.com/lambda) Function
+The function uses a simple JSON dictionary to create Slack Channel => Pivotal Project pairings.
 
-##Prerequisites
-1. AWS Account
-2. Slack Account
+So, for example, you could have a Slack channel named #devops which had a
+corresponding DevOps Pivotal Project. Quickly add stories to the project by
+simply typing "!pivotal clean up the code for the server" in the #devops channel.
+
+The code in this repo is designed to run as an [AWS Lambda Function](https://aws.amazon.com/lambda)
+
+## Prerequisites
+
+1. AWS account
+2. Slack admin account
 3. [awscli](https://aws.amazon.com/cli/) Installed
 
-##Steps
-1. Run the `bundleit.sh` script to zip up the code into bundle.zip
-2. Create a "blank" Lambda function named bangpivotal.
-3. Give bangpivotal an API Gateway with "Open" security.
-4. Give bangpivotal 2 environment variables:
-  1. slack_token (you get the value from your Slack outgoing webhook integration)
-  2. pivotal_token (you get the value from your PivotalTracker profile)
-5. Upload bundle.zip via awscli:  
-`aws lambda update-function-code --zip-file fileb://bundle.zip --function-name bangpivotal`
-6. Configure your Slack outgoing webhook to trigger on `!pivotal` and point it to your API Gateway URL.
+## The Short Instructions
+1.  Create a `dictionary.json` file to define channel:project pairings.
+1.  Upload the dictionary file to S3:
+  - `aws s3 cp dictionary.json s3://path/to/bucket --acl public-read`
+  - get the public URL for the uploaded file
+1. Create a Slack Incoming WebHook
+  - get the URL
+  - get the token
+1. Create a `bangpivotal` Lambda
+  - Add 4 environment variables
+    - `incoming_slack_url`
+    - `slack_token`
+    - `pivotal_token`
+    - `json_dictionary_url`
+  - Add an API Gateway trigger
+1. Create a Slack Outgoing WebHook
+  - get the token
+  - paste in the AWS API Gateway trigger URL
 
-##Notes
-These instructions are pretty hasty and assume you have at least some idea of how to use webhooks/AWS/Slack/etc.  
+
+## AWS Lambda Setup
+
+1. Clone the repo.
+1. Run the `bundleit.sh` script to zip up the code into a file named `bundle.zip`
+1. Log in to the [AWS Console Lambda service](https://console.aws.amazon.com/lambda/).
+2. Create a new Lambda function named `bangpivotal`.
+3. Add an API Gateway with "Open" security to `bangpivotal`.
+4. Add 3 environment variables to `bangpivotal`:
+
+  1. `slack_token` — You will get this value from your Slack outgoing webhook integration.
+  2. `pivotal_token` — You will get this value from your PivotalTracker profile.
+  3. `json_dictionary` — You will need to store your pairings dictionary in a  should be the URL to your JSON dictionary file.
+
+5. Upload bundle.zip via awscli:<br>
+  `aws lambda update-function-code --zip-file fileb://bundle.zip --function-name bangpivotal`
+6. Configure your Slack outgoing webhook to trigger on `!pivotal` and point it to your API Gateway URL.
+  ### Steps
+  1. <https://YOURSLACK.slack.com/apps/build/custom-integration>
+  2. Outgoing WebHooks > Add Outgoing WebHooks integration
+  3. Channel: Any
+  4. Trigger Word(s): !pivotal
+  5. URL(s): [the API Gateway URL of your Lambda Function]
+  6. Incoming WebHooks > Add Incoming WebHooks integration
+  7.
+
+## Notes
+
+These instructions are pretty hasty and assume you have at least some idea of how to use webhooks/AWS/Slack/etc.<br>
 Also my code is probably pretty ugly. Please feel free to pretty it up.
--- bangpivotal.py
+++ bangpivotal.py
@@ -1,18 +1,20 @@
 '''
-This function handles a Slack !pivotal command to add stories to pivotaltracker
+This function handles a Slack !pivotal command to add stories to pivotaltracker.
 
-You will need to set 2 Environment Variables in your AWS Lambda Function.
+You will need to set 4 Environment Variables in your AWS Lambda Function.
 
 Environment Variable            Value
 --------------------            -----
 slack_token                     Your Slack outgoing webhook token
 pivotal_token                   Your PivotalTracker API token
+incoming_slack_url              Your Slack incoming webhook URL
+json_dictionary_url             The URL to your dictionary.json file (S3 storage with public-read recommended)
 
 Steps:
 1) Run the script from the root of the git repo to create the bundle.zip
     chmod +x bundleit.sh && ./bundleit.sh
 2) Run the following aws command to upload bundle.zip to the lambda function
-    aws lambda update-function-code --zip-file fileb://bundle.zip --function-name FUNCTION_NAME    
+    aws lambda update-function-code --zip-file fileb://bundle.zip --function-name FUNCTION_NAME
 '''
 
 import json
@@ -23,20 +25,26 @@ import os
 import logging
 
 slack_token = os.environ['slack_token']
+incoming_slack_url = os.environ['incoming_slack_url']
 pivotal_token = os.environ['pivotal_token']
+json_dictionary_url = os.environ['json_dictionary_url']
 
-pivotal_headers = { "X-TrackerToken": pivotal_token, "Content-Type": "application/json" }
+pivotal_headers = {"X-TrackerToken": pivotal_token,
+                   "Content-Type": "application/json"}
 pivotal_url = "https://www.pivotaltracker.com/services/v5"
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 
+def get_pairings():
+    response = requests.get(json_dictionary_url)
+    return response.json()
+
+
 def help_response():
-    message = "You must specify a project name and a story name, separated by a semicolon.\n"
-    message += "Optional: additional description text, separated by a 2nd semicolon.\n"
-    message += "e.g. !pivotal IT; Buy more ethernet cables; We are running out\n"
-    message += "Valid projects include:\n" + list_project_names(get_projects_list())
+    message = "Specify a story name. Optional: add a description after a semicolon.\n"
+    message += "e.g. !pivotal Buy more ethernet cables; We are running out.\n"
     return message
 
 
@@ -50,91 +58,54 @@ def respond(err, res=None):
     }
 
 
-def get_projects_list():
-    response = requests.get(pivotal_url + "/projects", headers=pivotal_headers)
-    return response.json()
-
-
-def list_project_names(response_list):
-    projects = ""
-    for p in response_list:
-        projects += p['name'] + "\n"
-    return projects
-
-
-def find_project_id(name):
-    matches = []
-    names = []
-    for p in get_projects_list():
-        if re.match(name, p['name']):
-            matches.append(p['id'])
-            names.append(str(p['name']))
-    return {
-            'matches': matches,
-            'names': names
-            }
-
-
 def add_story(pid, name, description):
     post_url = "%s/projects/%s/stories" % (pivotal_url, pid)
     response = requests.post(post_url, headers=pivotal_headers, json={'name': name, 'description': description})
-    return response
+    return response.json()
 
 
 def parse_command_text(command_text):
-    # set some sensible defaults
-    pid = None
-    story = None
-    description = None
-    status = False
+    # message is only if there's an error or help
+    message = None
 
-    # check and correct formatting of command_text if necessary
-    if command_text.count(';') == 0:
-        return { 'status': status, 'message': help_response() }
-    elif command_text.count(';') == 1:
-        command_text += ";"
+    # check for blank or "help" command
+    split_text = command_text.split()
+    if len(split_text) == 0:
+        return {'status': False, 'message': help_response()}
+    if len(split_text) == 1 and split_text[0] == "help":
+        return {'status': False, 'message': help_response()}
 
-    # split up the command arguments
-    command_args = command_text.split(';')
-    project_ids = find_project_id(command_args[0].strip())
-    story = command_args[1].strip()
-    description = command_args[2].strip()
-
-    if len(project_ids['matches']) == 0:
-        return { 'status': status, 'message': help_response() }
-    elif len(project_ids['matches']) > 1:
-        message = "Sorry, you need to be more specific. Did you mean one of these?\n%s" % ", ".join(project_ids['names'])
+    # split story and description if there's a delimiter
+    if ";" in command_text:
+        command_args = command_text.split(';')
+        story = command_args[0].strip()
+        description = command_args[1].strip()
     else:
-        status = True
-        message = "Story '%s' added to Project '%s'" % (story, project_ids['names'][0])
-        pid = project_ids['matches'][0]
-    return { 'status': status, 'message': message, 'pid': pid, 'story': story, 'description': description}
+        story = command_text
+        description = None
+    return {'status': True, 'message': message, 'story': story, 'description': description}
 
 
 def lambda_handler(event, context):
-    # logger.info('got event: {}'.format(event))
-    # logger.error('something went wrong')
     params = parse_qs(event['body'])
-    # logger.info('got params: {}'.format(params))
     token = params['token'][0]
-    # logger.info('got token: {}'.format(token))
     if token != slack_token:
         logger.error("Request token (%s) does not match expected", token)
         return respond(Exception('Invalid request token'))
 
     user = params['user_name'][0]
     command = params['trigger_word'][0]
-    # logger.info('got command: {}'.format(command))
     channel = params['channel_name'][0]
+    pid = get_pairings()[channel]
     command_text = params['text'][0].replace(command, "", 1)
-    # logger.info('got command_text: {}'.format(command_text))
 
+    # action object = {status,message,story,description}
     action = parse_command_text(command_text)
 
     if action['status'] == True:
         story_name = "%s (from %s)" % (action['story'], user)
-        add_story(action['pid'], story_name, action['description'])
-        # logger.info(action['message'])
-        return respond(None, { 'text': action['message'], 'response_type': 'in_channel', 'user_name': 'PivotalTracker', 'icon_emoji': ':ghost:' } )
+        story_response = add_story(pid, story_name, action['description'])
+        message_text = "Story '%s' added!\n%s" % (action['story'], story_response['url'])
+        return respond(None, {'text': message_text , 'response_type': 'in_channel', 'user_name': 'PivotalTracker'})
     else:
-        return respond(None, { 'text': action['message'], 'response_type': 'in_channel', 'user_name': 'PivotalTracker', 'icon_emoji': ':ghost:' })
+        return respond(None, {'text': action['message'], 'response_type': 'in_channel', 'user_name': 'PivotalTracker'})
